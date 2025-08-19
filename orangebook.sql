aacf6cf0bee4b2eb1e967feae2dc9f55dc94457b
Steve Barron <sdbarron@users.noreply.github.com>
2025-08-18 23:03:57 -0400

    Update orangebook.sql

--- orangebook.sql
+++ orangebook.sql
@@ -1,6 +1,6 @@
 truncate table orangebook.products;
 truncate table orangebook.patent;
 truncate table orangebook.exclusivity;
-copy orangebook.products    from 'products.txt'    delimiter '~' header;
-copy orangebook.patent      from 'patent.txt'      delimiter '~' header;
-copy orangebook.exclusivity from 'exclusivity.txt' delimiter '~' header;
+copy orangebook.products    from './products.txt'    delimiter '~' header;
+copy orangebook.patent      from './patent.txt'      delimiter '~' header;
+copy orangebook.exclusivity from './exclusivity.txt' delimiter '~' header;
