739c077ffb8b7410dbf5ef08ffcc2d9d7ecd4b5d
Steve Barron <sdbarron@users.noreply.github.com>
2025-08-18 23:05:18 -0400

    Update orangebook.sh

--- orangebook.sh
+++ orangebook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -o ./orangebook.zip https://www.fda.gov/media/76860/download?attachment
+curl -o orangebook.zip https://www.fda.gov/media/76860/download?attachment
 unzip orangebook
 psql -d postgres -f orangebook.sql
