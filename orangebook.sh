4aa3b295d295012319ea88163e89c0e644be37d5
Steve Barron <sdbarron@users.noreply.github.com>
2025-09-18 21:34:55 -0400

    Specify postgres user in orangebook.sh

--- orangebook.sh
+++ orangebook.sh
@@ -2,4 +2,4 @@
 curl -o orangebook.zip https://www.fda.gov/media/76860/download?attachment
 PGPASSWORD=postgres
 unzip orangebook
-psql -d postgres -f orangebook.sql
+psql -U postgres -d postgres -f orangebook.sql
