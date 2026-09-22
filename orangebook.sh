36269eb8e1207be26afc79a3a2476bf5f044bf0d
Steve Barron <sdbarron@users.noreply.github.com>
2026-09-21 22:55:31 -0400

    Fix case sensitivity in chmod command for OrangeBook

--- orangebook.sh
+++ orangebook.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl -o orangebook.zip https://www.fda.gov/media/76860/download?attachment
 unzip orangebook
-chmod 777 ./orangebook/*.txt
+chmod 777 ./OrangeBook/*.txt
 psql -d postgres -f orangebook.sql
